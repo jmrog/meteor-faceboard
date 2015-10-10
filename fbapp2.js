1da8d7f15b7db6da54046a1d12162fe29519fd83
Hamilton Smith <smithhamilton4@gmail.com>
2015-10-10 15:40:06 -0400

    cleanup

--- fbapp2.js
+++ fbapp2.js
@@ -1,12 +1,12 @@
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
+// Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
+ /* Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
-    }
-  });
+    } 
+  }); */
 
   window.fbAsyncInit = function() {
     FB.init({
@@ -19,18 +19,13 @@ if (Meteor.isClient) {
 
 
 
- /* FB.ui(
-   {
-     method: 'feed'
-   }
-  );
- */
-  Template.hello.events({
+
+ /* Template.hello.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
     }
-  });
+  }); */
 }
 
 if (Meteor.isServer) {
