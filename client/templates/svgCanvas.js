3ad066da5bf7e1d3dc1d3d4143b125b7c50248f5
Jason Rogers <jason@ascendiv.com>
2015-10-10 17:21:26 -0400

    Stores line color along with coordinates

--- client/templates/svgCanvas.js
+++ client/templates/svgCanvas.js
@@ -4,10 +4,11 @@ Tracker.autorun(function() {
     canvasContainerOffset = Session.get('svgCanvasContainerOffset');
 });
 
-function insertCircle(x, y) {
+function insertCircle(x, y, lineColor) {
     Meteor.call('insertCirclePoint', {
         x: x - canvasContainerOffset.left,
-        y: y - canvasContainerOffset.top
+        y: y - canvasContainerOffset.top,
+        lineColor: lineColor
     });
 }
 
@@ -20,58 +21,32 @@ Template.svgCanvas.events({
     },
     'mousemove #svgCanvasContainer': function(evt) {
         if (Session.get('isDrawing')) {
-            insertCircle(evt.pageX, evt.pageY);
+            insertCircle(evt.pageX, evt.pageY, Session.get('lineColor'));
         }
     },
     'click button': function(evt) {
         evt.preventDefault();
         evt.stopPropagation();
 
-      switch  (evt.target.id) {
-        
-        case "clear":
+        switch  (evt.target.id) {
 
-          Meteor.call('clearCanvas', function () {
-            d3.select('#svgCanvasContainer svg').remove();
-            d3.select('#svgCanvasContainer').append('svg').attr('width', 500).attr('height', 500);
-        });
-          break;
+            case "clear":
+                Meteor.call('clearCanvas', function () {
+                    d3.select('#svgCanvasContainer svg').remove();
+                    d3.select('#svgCanvasContainer').append('svg').attr('width', 500).attr('height', 500);
+                });
+                break;
 
-        case "save":
-          Meteor.call('saveCanvas', function(err, canvasId) {
-            alert('Saved canvas with id: ' + canvasId);
-        });
-          break;
+            case "save":
+                Meteor.call('saveCanvas', function(err, canvasId) {
+                    alert('Saved canvas with id: ' + canvasId);
+                });
+                break;
 
+            default:
+                Session.set('lineColor', evt.target.id);
 
-        case "red" :
-          Session.set("lineColor", "red");
-          break;
-
-        case "blue" :
-          Session.set("lineColor", "blue");
-          break;
-
-        case "green" :
-          Session.set("lineColor", "green");
-          break;
-
-        case "orange" :
-          Session.set("lineColor", "orange");
-          break;
-
-        case "yellow" :
-          Session.set("lineColor", "yellow");
-          break;
-
-        case "white" :
-          Session.set("lineColor", "white");
-          break;
-
-        default: 
-          Session.set("lineColor", "black");
         }
-    },
-
+    }
 });
 
