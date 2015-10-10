2e5398a47f10fe20c569fa3f7b591671095e473c
discdiver <jeffmshale@gmail.com>
2015-10-10 17:05:05 -0400

    add colors

--- client/templates/svgCanvas.js
+++ client/templates/svgCanvas.js
@@ -27,9 +27,48 @@ Template.svgCanvas.events({
         evt.preventDefault();
         evt.stopPropagation();
 
-        Meteor.call('clearCanvas');
-        d3.select('#svgCanvasContainer svg').remove();
-        d3.select('#svgCanvasContainer').append('svg').attr('width', 500).attr('height', 500);
-    }
+        switch  (evt.target.id) {
+        
+        case "clear":
+
+          Meteor.call('clearCanvas');
+          d3.select('#svgCanvasContainer svg').remove();
+          d3.select('#svgCanvasContainer').append('svg').attr('width', 500).attr('height', 500);
+          break;
+
+        case "save":
+
+          break;
+
+
+        case "red" :
+          Session.set("lineColor", "red");
+          break;
+
+        case "blue" :
+          Session.set("lineColor", "blue");
+          break;
+
+        case "green" :
+          Session.set("lineColor", "green");
+          break;
+
+        case "orange" :
+          Session.set("lineColor", "orange");
+          break;
+
+        case "yellow" :
+          Session.set("lineColor", "yellow");
+          break;
+
+        case "white" :
+          Session.set("lineColor", "white");
+          break;
+
+        default: 
+          Session.set("lineColor", "black");
+        }
+    },
+
 });
 
